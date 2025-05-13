3a073838ed6c9e83ec3b79279f9acbbd298c6b0b
pabloredondoo <pabloredondolopez2005@gmail.com>
2025-05-13 12:59:24 +0200

    Metodo fibonacci y Metodo Factorial

--- algoritmo.java
+++ algoritmo.java
@@ -1,7 +1,7 @@
 public abstract class algoritmos {
-    
-    //Método Fibonacci
-    public static int fibonacci(int numero) {
+
+	// Método Fibonacci
+	public static int fibonacci(int numero) {
         if (numero < 0) {
             throw new IllegalArgumentException("El número debe ser mayor o igual que 0");
         }
@@ -23,7 +23,26 @@ public abstract class algoritmos {
 
        
         return siguiente;
-    }
-    
-}
+      //Método Factorial
+        public static long factorial(int numero) {
+            if (numero < 0) {
+                throw new IllegalArgumentException("El número debe ser mayor o igual que 0");
+            }
 
+
+            long resultado = 1;
+
+
+            for (int i = 2; i <= numero; i++) {
+                resultado *= i;
+            }
+
+
+            return resultado;
+        }
+
+
+
+    }
+
+}
