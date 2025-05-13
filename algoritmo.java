ed44d89f4bacc9af63b7d25e800f849c2af97a6e
pabloredondoo <pabloredondolopez2005@gmail.com>
2025-05-13 12:56:29 +0200

    Metodo fibonacci

--- algoritmo.java
+++ algoritmo.java
@@ -1,4 +1,29 @@
+public abstract class algoritmos {
+    
+    //Método Fibonacci
+    public static int fibonacci(int numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("El número debe ser mayor o igual que 0");
+        }
+        if (numero == 0) return 0;
+        if (numero == 1) return 1;
 
-public class algoritmo {
 
+        int anterior = 0;
+        int actual = 1;
+        int siguiente = 1;
+
+
+        for (int i = 2; i <= numero; i++) {
+            siguiente = anterior + actual;
+            anterior = actual;
+            actual = siguiente;
+        }
+
+
+       
+        return siguiente;
+    }
+    
 }
+
